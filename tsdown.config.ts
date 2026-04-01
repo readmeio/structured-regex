6e1f8dcf3dab304fefd0c381e5ced4fd9a00a2cf
Jon Ursenbach <jon@ursenba.ch>
2026-04-01 09:11:51 -0700

    fix: ts6, tsdown and node 22

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,6 @@
-import type { Options } from 'tsup';
+import { defineConfig } from 'tsdown';
 
-import { defineConfig } from 'tsup';
-
-export default defineConfig((options: Options) => ({
+export default defineConfig(options => ({
   ...options,
 
   cjsInterop: true,
